a790f5bf6bc06780108a89bab988d27843d0885e
aginamena5 <aginamena5@gmail.com>
2021-11-06 09:34:45 -0400

    users are able to now view video when hover on viewers div

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -2,14 +2,54 @@ import React from 'react'
 import "./Viewers.css";
 
 function Viewers() {
+    function playVideo(event) {
+        if (event.target.tagName === "IMG") {
+            const targetImg = event.target;
+            targetImg.style = "display:none";
+            let video = targetImg.nextSibling;
+            video.style = "display:block"
+            video.play();
+        }
+    }
+    function stopVideo(event) {
+        if (event.target.tagName === "VIDEO") {
+            const targetVideo = event.target;
+            targetVideo.previousSibling.style = "display:block";
+            targetVideo.style = "display:none";
+        }
+    }
+
     return (
+
         <div id="viewers" className="container">
-            <img src="./images/viewers-disney.png" alt="disney" />
-            <img src="./images/viewers-pixar.png" alt="pixar" />
-            <img src="./images/viewers-marvel.png" alt="marvel" />
-            <img src="./images/viewers-starwars.png" alt="starwars" />
-            <img src="./images/viewers-national.png" alt="national" />
+
+            <div onMouseOver={playVideo} onMouseOut={stopVideo} className="test" >
+
+                <img src="./images/viewers-disney.png" alt="disney" id="video1" />
+                <video src="./videos/1564674844-disney.mp4" width="200" height="100" controls loop />
+            </div>
+            <div onMouseOver={playVideo} onMouseOut={stopVideo} className="test">
+
+                <img src="./images/viewers-pixar.png" alt="pixar" id="video1" />
+                <video src="./videos/1564676714-pixar.mp4" width="200" height="100" controls loop />
+            </div>
+            <div onMouseOver={playVideo} onMouseOut={stopVideo} className="test">
+
+                <img src="./images/viewers-marvel.png" alt="marvel" id="video1" />
+                <video src="./videos/1564676115-marvel.mp4" width="200" height="100" controls loop />
+            </div>
+            <div onMouseOver={playVideo} onMouseOut={stopVideo} className="test">
+
+                <img src="./images/viewers-starwars.png" alt="starwars" id="video1" />
+                <video src="./videos/1608229455-star-wars.mp4" width="200" height="100" controls loop />
+            </div>
+            <div onMouseOver={playVideo} onMouseOut={stopVideo} className="test">
+
+                <img src="./images/viewers-national.png" alt="national" id="video1" />
+                <video src="./videos/1564676296-national-geographic.mp4" width="200" height="100" controls loop />
+            </div>
         </div>
+
     )
 }
 
